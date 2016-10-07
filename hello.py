6690e068d3f05455b8df46744edfa860bb974840
Erodriguez01 <rodrigueze394@yahoo.com>
2016-10-07 11:15:32 -0500

    create hello.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+##############################################
+#
+# Name: Rodriguez
+# COurse: 1331
+# Origin Date: 9/02/16
+# Description: Hello World
+# This Program we play with "while"
+#
+##############################################
+
+i=0
+print("start, i= " + str(i))
+while(i <= 3):
+    # The next line should print "hello World " when the program is run
+    print ("Hello World, i= " + str(i))
+    i=i+1
+
+print ("Done, i= " + str(i))
